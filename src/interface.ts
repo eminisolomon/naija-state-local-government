f9e7043d2a98f1f78ea9f32b2ae7b13b6bfe4de9
Solomon Olatunji <aotoluwalope@gmail.com>
2025-12-05 08:44:28 +0100

    refactor: remove JSDoc comments from `State` and `StateWithCapital` interfaces and delete `StateData` interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,30 +1,14 @@
-/**
- * Represents a Nigerian state with complete location data
- */
 export interface State {
-  /** Name of the state */
   state: string;
-  /** Capital city of the state */
   capital: string;
-  /** List of local government areas in the state */
   lgas: string[];
-  /** List of senatorial districts in the state */
   senatorial_districts: string[];
-  /** List of major towns in the state */
   towns: string[];
 }
 
-/**
- * Response type for state data queries
- */
 export interface StateData extends State {}
 
-/**
- * Simplified state with capital information
- */
 export interface StateWithCapital {
-  /** Name of the state */
   state: string;
-  /** Capital city of the state */
   capital: string;
 }
